52e9343ed0a1b767d4c1e574c2454c89c71bef1f
Tim Martin <git@timmartin.me>
2019-09-26 23:40:08 -0400

    checkpoint

--- setup.py
+++ setup.py
@@ -3,27 +3,18 @@
 
 from setuptools import setup
 
-with open("README.md") as readme_file:
-    readme = readme_file.read()
-
-with open("HISTORY.md") as history_file:
-    history = history_file.read()
-
 requirements = ["psycopg2", "python-rapidjson", "flask", "werkzeug"]
 
 setup(
     name="hou-flask-psycopg2",
     version="0.0.3",
     description="Highly Opinionated Utils: Flask Psycopg2",
-    long_description=readme + "\n\n" + history,
+    long_description="TODO",
     author="Tim Martin",
     author_email="oss@timmartin.me",
     url="https://github.com/timmartin19/hou-flask-psycopg2",
     packages=["hou_flask_psycopg2"],
     package_dir={"hou_flask_psycopg2": "hou_flask_psycopg2"},
-    package_data={
-        "hou-flask-psycopg2": ["README.md", "HISTORY.md"]
-    },
     include_package_data=True,
     install_requires=requirements,
     zip_safe=False,
